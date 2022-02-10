c636aef0fc4923a35789a3c38f7d24b938dab746
tapia1013 <manbearpig1013@gmail.com>
2022-02-10 11:53:36 -0800

    login and user routes with controllers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to support desk API' })
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`))
