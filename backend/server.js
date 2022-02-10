83e3be00c5ef591fe7ac6a6b111053b97932f3f1
tapia1013 <manbearpig1013@gmail.com>
2022-02-10 12:21:55 -0800

    errorMiddlware asyncHandler

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// allows us to send raw json
+app.use(express.json());
+// accept url encoded form
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to support desk API' })
@@ -11,6 +17,9 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+// Error handler
+app.use(errorHandler)
 
 
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`))
