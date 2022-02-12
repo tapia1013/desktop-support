282246cf4b229ac20826048018ead26f93a3c112
tapia1013 <manbearpig1013@gmail.com>
2022-02-11 19:11:46 -0800

    tickets but issue with routes its not showing in redux but does in compass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,9 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import NewTicket from './pages/NewTicket';
 import PrivateRoute from './components/PrivateRoute';
+import NewTicket from './pages/NewTicket';
+import Tickets from './pages/Tickets';
 
 
 
@@ -23,6 +24,9 @@ function App() {
             <Route path='/register' element={<Register />} />
             <Route path='/new-ticket' element={<PrivateRoute />}>
               <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
+            <Route path='/tickets' element={<PrivateRoute />}>
+              <Route path='/tickets' element={<Tickets />} />
             </Route>
           </Routes>
         </div>
--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -16,8 +16,24 @@ const createTicket = async (ticketData, token) => {
 }
 
 
+// Get user tickets
+const getTickets = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+
+  const response = await axios.get(API_URL, config);
+
+  return response.data;
+}
+
+
+
 const ticketService = {
-  createTicket
+  createTicket,
+  getTickets,
 }
 
 export default ticketService;
